8bc1dc94f59122329a0be3460c94d8355e7835ac
mtaha96 <mtaha96@my.yorku.ca>
2019-07-10 20:38:41 -0400

    Fix issue with production enviroment keys

--- config/prod.js
+++ config/prod.js
@@ -6,5 +6,5 @@ module.exports = {
   stripePublishableKey: process.env.STRIPE_PUBLISHABLE_KEY,
   stripeSecretKey: process.env.STRIPE_SECRET_KEY,
   sendGridKey: process.env.SENDGRID_KEY,
-  redirectDomain:
+  redirectDomain: process.env.REDIRECT_DOMAIN
 };
